22054f97ac6c7fe545794fad608865bd2627b5cd
rdubouski <renics108>
2021-11-21 16:31:53 +0300

    Lesson 2

---
+++ src/U1_L2.java
@@ -0,0 +1,13 @@
+public class U1_L2 {
+    public static void main(String[] args) {
+        int var1;
+        int var2;
+
+        var1 = 1024;
+        System.out.println("var1 = " + var1);
+
+        var2 = var1 / 2;
+        System.out.println("var1 / 2 = " + var2);
+
+    }
+}
