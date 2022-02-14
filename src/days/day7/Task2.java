85b3c7d6493b53476e3e042ae9b989dc841112b8
rdubouski <renics108>
2022-02-14 19:47:55 +0300

    Day7 complete

---
+++ src/days/day7/Task2.java
@@ -0,0 +1,59 @@
+package days.day7;
+
+class Player {
+    private int stamina;
+    private static final int MAX_STAMINA = 100;
+    private static final int MIN_STAMINA = 0;
+    private static int countPlayers = 0;
+
+    public Player(int stamina) {
+        this.stamina = stamina;
+        if (countPlayers < 6) countPlayers++;
+    }
+
+    public int getStamina() {
+        return  stamina;
+    }
+
+    public void run() {
+        if (this.stamina > MIN_STAMINA) {
+            this.stamina = this.stamina - 1;
+        }
+        if (this.stamina == 0) {
+            countPlayers--;
+        }
+    }
+
+    public static void info() {
+        if(countPlayers < 6) {
+            System.out.println("Teams not full. Last " + (6 - countPlayers) + " free place");
+        } else {
+            System.out.println("Teams is full");
+        }
+    }
+
+}
+
+public class Task2 {
+    public static void main(String[] args) {
+        Player p1 = new Player(90);
+        Player p2 = new Player(91);
+        Player p3 = new Player(92);
+        Player p4 = new Player(93);
+        Player p5 = new Player(94);
+        Player p6 = new Player(95);
+        Player.info();
+
+        Player p7 = new Player(96);
+        Player p8 = new Player(97);
+        Player.info();
+
+        while (p6.getStamina() != 0){
+            p6.run();
+        }
+        Player.info();
+
+        Player p9 = new Player(98);
+        Player.info();
+    }
+}
